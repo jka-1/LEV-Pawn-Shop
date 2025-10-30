50db90f573f4338875d05a0844ab9d08584ccf26
Jay <ja310331@ucf.edu>
2025-10-29 23:15:35 -0400

    Adding code from Shiven for Swift UI

---
+++ apps/ios/BigProjectUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigProjectUIApp.swift
+//  BigProjectUI
+//
+//  Created by Shiven Rahul on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BigProjectUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
